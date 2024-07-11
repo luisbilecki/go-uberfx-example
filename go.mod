40910d477148c93f3eac6f90913e1b78e8af0085
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2024-07-10 21:14:09 -0300

    feat: add a zap logger

--- go.mod
+++ go.mod
@@ -2,11 +2,13 @@ module github.com/luisbilecki/go-uberfx-example
 
 go 1.22.5
 
-require go.uber.org/fx v1.22.1
+require (
+	go.uber.org/fx v1.22.1
+	go.uber.org/zap v1.26.0
+)
 
 require (
 	go.uber.org/dig v1.17.1 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
 )
--- main.go
+++ main.go
@@ -7,11 +7,21 @@ import (
 	routes "github.com/luisbilecki/go-uberfx-example/route"
 	"github.com/luisbilecki/go-uberfx-example/service"
 	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
+	"go.uber.org/zap"
 )
 
 func main() {
 	fx.New(
-		fx.Provide(service.NewHTTPServer, routes.NewEchoRoute, handler.NewEchoHandler),
+		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
+			return &fxevent.ZapLogger{Logger: log}
+		}),
+		fx.Provide(
+			service.NewHTTPServer,
+			routes.NewEchoRoute,
+			handler.NewEchoHandler,
+			zap.NewProduction,
+		),
 		fx.Invoke(func(*http.Server) {}),
 	).Run()
 }
